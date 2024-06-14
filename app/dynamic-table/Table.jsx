0d1e028062be7bf27e138fcea46922e8d6a256e3
Sumant Shah <sumant.shah@gigacars.com>
2024-06-14 15:34:03 +0530

    tailwind style

--- app/dynamic-table/Table.jsx
+++ app/dynamic-table/Table.jsx
@@ -28,31 +28,14 @@ const Table = () => {
 
     }, []);
 
-    // const handleColumn = (item) => {
-    //     setLoading(true);
-    //     console.log(item);
-    //     let newCols = columns;
-    //     if (newCols.includes(item)) {
-    //         let filterData = newCols.filter((col) => {
-    //             return col !== item
-    //         })
-    //         setColumns(filterData)
-    //     } else {
-    //         newCols.push(item);
-    //         setColumns(newCols)
-    //     }
-    //     setLoading(false)
-    // }
-
     const handleColumn = (item) => {
         console.log(item)
         setLoading(true);
         let newCols = columns;
-        if(newCols[item]){
-            console.log('entered in true')
-            setColumns({...columns, [item]: false})
+        if (newCols[item]) {
+            setColumns({ ...columns, [item]: false })
         } else {
-            setColumns({...columns, [item]: true})
+            setColumns({ ...columns, [item]: true })
         }
 
         setLoading(false);
@@ -61,30 +44,31 @@ const Table = () => {
     const handleSearch = (event) => {
         setLoading(true);
         setSearchClient(event.target.value);
-        if(event.target.value){
+        if (event.target.value) {
             console.log(event.target.value)
             let searchRows = mydata.filter((item) => {
                 let regex = new RegExp(event.target.value, "i", "g")
                 return item.payer.toString().match(regex)
             })
-    
-            console.log(searchRows)
             setRows([...searchRows])
-            } else {
-                setRows(mydata)
-                }
-                
-                setLoading(false);
+        } else {
+            setRows(mydata)
+        }
+
+        setLoading(false);
     }
 
 
     return (
-        <div>
+        <div className='m-4'>
             <h1>Dynamic Table</h1>
-            <input type="text" placeholder='Search Client' value={searchClient} onChange={(event) => handleSearch(event)} />
+            <div className='text-right'>
+                <input className='border-2 ' type="text" placeholder='Search Client' value={searchClient} onChange={(event) => handleSearch(event)} />
+
+            </div>
             <div style={{ position: 'relative' }}>
                 <button onClick={() => setDialog(!dialog)}>Dynamic columns</button>
-                <div className={`column__dialog ${dialog && 'active'}`}>
+                <div className={`border-2 bg-[#fff] shadow-lg absolute ${dialog ? 'block' : 'hidden'}`}>
                     {
                         Object.keys(columnData).map((item) => {
                             return (
@@ -96,45 +80,51 @@ const Table = () => {
                     }
                 </div>
             </div>
-            {
-                !loading && 
-                    <table>
-                        <thead>
-                            <tr>
+            <div className='flex justify-center'>
+                <div className='w-[90%] overflow-auto'>
+
+                    {
+                        !loading &&
+                        <table className='border-2  w-full mt-3'>
+                            <thead className='border-2 pt-5 pb-5 text-start'>
+                                <tr>
+                                    {
+                                        Object.keys(columns).map((item) => {
+
+                                            if (columns[item]) {
+                                                return (
+                                                    <th className='text-start'>{item}</th>
+                                                )
+
+                                            }
+
+                                        })
+                                    }
+                                </tr>
+                            </thead>
+                            <tbody>
                                 {
-                                    Object.keys(columns).map((item) => {
+                                    rows.map((item) => {
 
-                                        if(columns[item]){
                                         return (
-                                            <th>{item}</th>
+                                            <tr className='pt-2 border-b-2'>
+                                                {columns['Created On'] && <td className='w-[300px]'>{item.createdOn}</td>}
+                                                {columns['Payer'] && <td className='w-[300px]'>{item.payer}</td>}
+                                                {columns['Status'] && <td className='w-[300px]'>{item.status}</td>}
+                                                {columns['Email'] && <td className='w-[300px]'>{item.email}</td>}
+                                                {columns['Services'] && <td className='w-[300px]'>{item.services}</td>}
+                                                {columns['Scheduled'] && <td className='w-[300px] pr-2'>{item.scheduled}</td>}
+                                                {columns['Payer Phone'] && <td className='w-[300px]'>{item.payerPhone}</td>}
+                                            </tr>
                                         )
-
-                                        }
-
                                     })
                                 }
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                rows.map((item) => {
+                            </tbody>
+                        </table>
+                    }
+                </div>
 
-                                    return (
-                                        <tr>
-                                            {columns['Created On'] && <td>{item.createdOn}</td>}
-                                            {columns['Payer'] && <td>{item.payer}</td>}
-                                            {columns['Status'] && <td>{item.status}</td>}
-                                            {columns['Email'] && <td>{item.email}</td>}
-                                            {columns['Services'] && <td>{item.services}</td>}
-                                            {columns['Scheduled'] && <td>{item.scheduled}</td>}
-                                            {columns['Payer Phone'] && <td>{item.payerPhone}</td>}
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-            }
+            </div>
         </div>
     )
 }
