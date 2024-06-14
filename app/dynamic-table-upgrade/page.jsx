a46d5e175ea9d7a91506861b62b08bc9b125d188
Sumant Shah <sumant.shah@gigacars.com>
2024-06-14 21:27:06 +0530

    dynamic table upgrade version using array

---
+++ app/dynamic-table-upgrade/page.jsx
@@ -0,0 +1,205 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import mydata from '../../data/data.json';
+import '../dynamic-table/style.css';
+
+const columnData = [
+    {
+        field: 'ID',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Created On',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Payer',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Status',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Email',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Services',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Scheduled',
+        display: true,
+        svg: ''
+    },
+    {
+        field: 'Payer Phone',
+        display: true,
+        svg: ''
+    }
+]
+
+const DynamicTableUpgrade = () => {
+    const [rows, setRows] = useState(mydata);
+    const [columns, setColumns] = useState(columnData);
+    const [dialog, setDialog] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [searchClient, setSearchClient] = useState('');
+    const [page, setPage] = useState(1);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+
+
+    useEffect(() => {
+
+    }, []);
+
+    const handleColumn = (item) => {
+        console.log(item)
+        setLoading(true);
+        let newCols = columns;
+        if (item.display) {
+            setColumns(columns.map((val) => {
+                if(val.field === item.field){
+
+                    val.display = false
+                }
+                return val;
+            }))
+        } else {
+            setColumns(columns.map((val) => {
+                if(val.field === item.field){
+
+                    val.display = true
+                }
+                return val;
+            }))
+            
+        }
+
+        setLoading(false);
+    }
+
+    const handleSearch = (event) => {
+        setLoading(true);
+        setSearchClient(event.target.value);
+        if (event.target.value) {
+            console.log(event.target.value)
+            let searchRows = mydata.filter((item) => {
+                let regex = new RegExp(event.target.value, "i", "g")
+                return item.payer.toString().match(regex)
+            })
+            setRows([...searchRows])
+        } else {
+            setRows(mydata)
+        }
+
+        setLoading(false);
+    }
+
+    const selectPageHandler = (selectPage) => {
+        if(selectPage >= 1 && selectPage <= rows.length / rowsPerPage && selectPage !== page) {
+            setPage(selectPage);
+        }
+    }
+
+
+    return (
+        <div className='m-4'>
+            <h1>Dynamic Table</h1>
+            <div className='text-right'>
+                <input className='border-2 rounded-xl p-[6px]' type="text" placeholder='Search Client' value={searchClient} onChange={(event) => handleSearch(event)} />
+
+            </div>
+            <div style={{ position: 'relative' }}>
+                <button onClick={() => setDialog(!dialog)}>Dynamic columns</button>
+                <div className={`border-2 bg-[#fff] shadow-lg absolute ${dialog ? 'block' : 'hidden'}`}>
+                    {
+                        columnData.map((item) => {
+                            return (
+                                <div onClick={() => handleColumn(item)}>
+                                    {item.field}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className='flex justify-center'>
+                <div className='w-[100%] overflow-auto'>
+
+                    {
+                        !loading &&
+                        <table className='rounded-xl border-2 w-full mt-3'>
+                            <thead className='border-b-2 text-start'>
+                                <tr>
+                                    {
+                                        columns.map((item) => {
+                                            if(item.display) {
+                                                return (
+                                                    <th className='text-start p-2 bg-slate-100'>{item.field}</th>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    rows.slice(page * rowsPerPage - rowsPerPage, page * rowsPerPage).map((item) => {
+
+                                        return (
+                                            <tr className='border-b-2'>
+                                                <td className='p-2'>{item.id}</td>
+                                                {columns[0].display && <td className='w-[300px]'>{item.createdOn}</td>}
+                                                {columns[1].display && <td className='w-[300px]'>{item.payer}</td>}
+                                                {columns[2].display && <td className='w-[300px]'>{item.status}</td>}
+                                                {columns[3].display && <td className='w-[300px]'>{item.email}</td>}
+                                                {columns[4].display && <td className='w-[300px]'>{item.services}</td>}
+                                                {columns[5].display && <td className='w-[300px] pr-2'>{item.scheduled}</td>}
+                                                {columns[6].display && <td className='w-[300px]'>{item.payerPhone}</td>}
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    }
+                </div>
+
+            </div>
+
+            <div className='flex justify-between'>
+                <div className="rows">
+                    
+                </div>
+
+                {
+                    rows.length > 0 &&
+                    <div className='p-[10px] mt-[10px] mb-[10px] flex justify-center'>
+                        <span onClick={() => selectPageHandler(page - 1)} className='border-2 p-3 cursor-pointer'>Prev</span>
+                        {
+                            [...Array(rows.length / rowsPerPage)].map((_, i) => {
+                                if(i === (page-1) - 1 || i === (page-1) || i === (page-1) + 1){
+                                return <span key={i} className={`p-2 pl-4 pr-4 border-2 ${page === i+1 ? 'bg-[#e2e2e2]': ''}`}>
+                                    {i + 1}
+                                </span>
+                                }
+                            })
+                        }
+                        <span onClick={() => selectPageHandler(page + 1)} className='border-2 p-3 cursor-pointer'>Next</span>
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default DynamicTableUpgrade
