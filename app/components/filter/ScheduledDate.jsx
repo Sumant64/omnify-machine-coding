09b8b0b20ecdb45ef166fdb3b53078342268bfd0
Sumant Shah <sumant.shah@gigacars.com>
2024-06-17 17:33:03 +0530

    scheduled date filter changes

--- app/components/filter/ScheduledDate.jsx
+++ app/components/filter/ScheduledDate.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 
-const ScheduledDate = ({scheduledDate, setScheduledDate}) => {
+const ScheduledDate = ({ scheduledDate, setScheduledDate }) => {
   const [dateDropdown, setDateDropdown] = useState('');
-  const [customDate, setCustomDate] = useState({from: '', to: ''});
+  const [customDate, setCustomDate] = useState({ from: '', to: '' });
   const [isCustom, setIsCustom] = useState(false)
   const scheduledDateFilter = useSelector((state) => state.filterReducer.filters.scheduledDate);
 
   useEffect(() => {
     console.log(scheduledDateFilter)
-    if(scheduledDateFilter.length > 0) {
+    if (scheduledDateFilter.length > 0) {
       setDateDropdown(scheduledDateFilter[2]);
       setScheduledDate(scheduledDateFilter)
-    }else {
+    } else {
       setDateDropdown('All')
     }
   }, [])
@@ -20,10 +20,13 @@ const ScheduledDate = ({scheduledDate, setScheduledDate}) => {
   const handleChange = (option) => {
     let from = '';
     let to = '';
+    if (option !== 'Custom') {
+      setIsCustom(false)
+    }
     setDateDropdown(option)
-    switch(option) {
+    switch (option) {
       case 'All':
-        setScheduledDate({from: '', to: '', field: 'All'})
+        setScheduledDate({ from: '', to: '', field: 'All' })
         break;
       case 'Custom':
         setIsCustom(true);
@@ -32,13 +35,13 @@ const ScheduledDate = ({scheduledDate, setScheduledDate}) => {
         to = new Date();
         from = new Date().setDate(to.getDate() - 30);
         from = new Date(from);
-        setScheduledDate({from, to, field: '30Days'});
+        setScheduledDate({ from, to, field: '30Days' });
         break;
       case 'ThisMonth':
         from = new Date().setDate(1);
         from = new Date(from);
         to = new Date();
-        setScheduledDate({from, to, field: 'ThisMonth'})
+        setScheduledDate({ from, to, field: 'ThisMonth' })
         break;
       case 'LastMonth':
         break;
@@ -50,41 +53,51 @@ const ScheduledDate = ({scheduledDate, setScheduledDate}) => {
         let currentYr = new Date().getFullYear();
         from = new Date(`01-01-${currentYr}`);
         to = new Date(`12-31-${currentYr}`);
-        setScheduledDate({from, to, field: 'ThisYear'});
+        setScheduledDate({ from, to, field: 'ThisYear' });
         break;
       case 'LastYear':
         let yr = new Date().getFullYear() - 1;
         to = new Date(`01-01-${yr}`)
         from = new Date(`12-31-${yr}`);
-        setScheduledDate({from, to, field: 'LastYear'})
+        setScheduledDate({ from, to, field: 'LastYear' })
         break;
     }
   }
-  
+
 
   return (
-    <div>
-      <select value={dateDropdown} onChange={(event) => handleChange(event.target.value)}>
-        <option value="All">All</option>
-        <option value="Custom">Custom</option>
-        <option value="30Days">Last 30 Days</option>
-        <option value="ThisMonth">This month</option>
-        <option value="LastMonth">Last month</option>
-        <option value="ThisQuarter">This quarter</option>
-        <option value="2QuarterAgo">2 quarter ago</option>
-        <option value="ThisYear">This Year</option>
-        <option value="LastYear">Last Year</option>
-      </select>
+    <div className='m-3 ml-4 mr-4'>
+      <h2>Show orders for</h2>
+      <div className='flex justify-center mt-3'>
+        <select className='border-2 w-[100%] p-2 rounded-lg shadow-sm' value={dateDropdown} onChange={(event) => handleChange(event.target.value)}>
+          <option value="All">All</option>
+          <option value="Custom">Custom</option>
+          <option value="30Days">Last 30 Days</option>
+          <option value="ThisMonth">This month</option>
+          <option value="LastMonth">Last month</option>
+          <option value="ThisQuarter">This quarter</option>
+          <option value="2QuarterAgo">2 quarter ago</option>
+          <option value="ThisYear">This Year</option>
+          <option value="LastYear">Last Year</option>
+        </select>
 
-      { isCustom && <div className="custom">
-        <input placeholder='From' type="date" value={customDate.from} onChange={(event) => {
-          setCustomDate({...customDate, from: event.target.value});
-          setScheduledDate({from: event.target.value, to: customDate.to, field: 'Custom'})
-        }} />
-        <input placeholder='To' type="date" value={customDate.to} onChange={(event) => {
-          setCustomDate({...customDate, to: event.target.value});
-          setScheduledDate({from: customDate.from, to: event.target.value, field: 'Custom'})
-        }} />
+      </div>
+      {isCustom && <div className="custom mt-6 flex">
+        <div className='flex-1'>
+          <h2>From</h2>
+          <input className='border-2 w-[90%] p-2 rounded-lg shadow-sm' placeholder='From' type="date" value={customDate.from} onChange={(event) => {
+            setCustomDate({ ...customDate, from: event.target.value });
+            setScheduledDate({ from: event.target.value, to: customDate.to, field: 'Custom' })
+          }} />
+        </div>
+
+        <div className='flex-1'>
+          <h2>To</h2>
+          <input className='border-2 w-[90%] p-2 rounded-lg shadow-sm' placeholder='To' type="date" value={customDate.to} onChange={(event) => {
+            setCustomDate({ ...customDate, to: event.target.value });
+            setScheduledDate({ from: customDate.from, to: event.target.value, field: 'Custom' })
+          }} />
+        </div>
       </div>}
     </div>
   )
